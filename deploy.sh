67264df3367a96301acae6a1a44dcb0917391ec1
enidev911 <enidev911@gmail.com>
2024-10-19 00:13:52 -0300

    change repo

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ git init
 git checkout -b main
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:enidev911/primsoftweb.git main:gh-pages
+git push -f git@github.com:primsoft-group/primsoftweb.git main:gh-pages
 
 cd -
